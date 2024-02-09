0ef6333308bd915b4f4e1fa44d92f8dc8de0c8cb
Haile <haile.tasma@runi.ac.il>
2024-02-09 19:12:26 +0200

    we good

--- PlayList.java
+++ PlayList.java
@@ -219,7 +219,7 @@ class PlayList {
                     min=tracks[i].duration;
                      helper = i;                       
         }
-    return helper + start-1;
+    return helper + start;
 }
 
 
